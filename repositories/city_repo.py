8fc4799e359d237d7c6fb7c866b2b9abdaf4313d
Matheus Vidal <matheusvidaldemenezes@gmail.com>
2023-11-10 00:37:39 -0300

    Add city repository

---
+++ repositories/city_repo.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from sqlalchemy import select, and_
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from database.models import City
+from repositories.sql_repo import SqlRepository
+
+
+class CityRepository(SqlRepository):
+    def __init__(self, db: AsyncSession) -> None:
+        super().__init__(db, City)
+
+    async def get(self, ids: List[int] = None, name: str = None, state_abbreviation: str = None) -> List[City]:
+        filters = []
+
+        if ids:
+            filters.append(City.id.in_(ids))
+
+        if name:
+            filters.append(City.name.istartswith(name))
+
+        if state_abbreviation:
+            filters.append(City.state_abbreviation.istartswith(state_abbreviation))
+
+        objs = await self.db.execute(select(self.model).where(and_(*filters)))
+        return objs.scalars().all()
