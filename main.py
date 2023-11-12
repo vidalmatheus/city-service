a09759da881c446fda9497d97db49b958dd17a11
Matheus Vidal <matheusvidaldemenezes@gmail.com>
2023-11-11 23:53:38 -0300

    Redirect to docs

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 from routes import city_routes, status_routes
 
@@ -6,3 +7,9 @@ app = FastAPI()
 
 app.include_router(status_routes.router, prefix="/status")
 app.include_router(city_routes.router, prefix="/city")
+
+
+# Redirect to /docs
+@app.get("/")
+async def redirect_to_docs():
+    return RedirectResponse("/docs")
