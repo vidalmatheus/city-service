5923db7180a8c00f3f7dba6d8ff54a751f3af0bc
Matheus Vidal <matheusvidaldemenezes@gmail.com>
2023-11-12 23:06:27 -0300

    Add CORSMiddleware

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 
 from routes import city_routes, status_routes
 
 app = FastAPI()
+
+
+app.add_middleware(CORSMiddleware, allow_origins=["*"])
+
 
 app.include_router(status_routes.router, prefix="/status")
 app.include_router(city_routes.router, prefix="/city")
